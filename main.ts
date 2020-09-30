180e7b115c31bb782232eff7c9779a4821fe3768
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-09-30 13:06:49 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+let Delay = 1000
+strip.clear()
+basic.showIcon(IconNames.Yes)
+strip.show()
 basic.forever(function () {
-	
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    basic.pause(Delay)
+    strip.show()
+    strip.clear()
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
+    basic.pause(Delay)
+    strip.show()
+    strip.clear()
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    basic.pause(Delay)
+    strip.show()
+    strip.clear()
 })
